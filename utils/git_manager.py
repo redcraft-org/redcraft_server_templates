7bc4c34d1ce09b6c1e576be04d18a55553d51490
lululombard <lululombard@gmail.com>
2021-01-24 11:55:26 -0500

    Switch back to branch after deleting the git manager

--- utils/git_manager.py
+++ utils/git_manager.py
@@ -2,19 +2,25 @@ import os
 
 from git import Repo
 
+
 class GitManager():
 
     def __init__(self):
         self.repo_location = os.path.join(os.path.dirname(__file__), '..')
         self.repo = Repo(self.repo_location)
+        self.original_branch = self.repo.active_branch
 
-    def use_update_branch(self, branch):
+    def __del__(self):
+        self.switch_branch(self.original_branch)
+
+    def switch_branch(self, branch):
         if branch not in self.repo.branches:
             self.repo.git.checkout('HEAD', b=branch)
         else:
             self.repo.git.checkout(branch)
 
     def push_and_create_pull_request(self):
-        templates_location = os.path.join(self.repo_location, 'templates', '*', 'rcst_template_*.json')
+        templates_location = os.path.join(
+            self.repo_location, 'templates', '*', 'rcst_template_*.json')
         self.repo.index.add([templates_location])
         return True
